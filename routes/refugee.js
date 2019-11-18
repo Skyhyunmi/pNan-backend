b6832e3a2fb4d0450281ad98f15fa99a9188c1b3
Jihwan Kim <noneobj@gmail.com>
2019-11-18 20:59:54 +0900

    Change eslint configuration

--- routes/refugee.js
+++ routes/refugee.js
@@ -4,19 +4,19 @@ const db = require('../models/index');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  //결과 없을 시 404 필요
+  // 결과 없을 시 404 필요
   db.Refugee.findAll().then(function (results) {
     res.json(results);
-  }).catch( function(err){
+  }).catch(function(err) {
     res.json(err);
   });
 });
 
 router.get('/:id', function(req, res) {
-  //결과 없을 시 404 필요
-  db.Refugee.findOne({where: {id: req.params.id}}).then(function (results) {
+  // 결과 없을 시 404 필요
+  db.Refugee.findOne({ where: { id: req.params.id } }).then(function (results) {
     res.json(results);
-  }).catch( function(err){
+  }).catch(function(err) {
     res.json(err);
   });
 });
@@ -25,10 +25,10 @@ router.post('/', function(req, res) {
   // 400 handling 필요
   const data = req.body;
   db.Refugee.create({
-    name: data['name'],
-    birth: data['birth'],
-    nationality: data['nationality'],
-    status: data['status'],
+    name: data.name,
+    birth: data.birth,
+    nationality: data.nationality,
+    status: data.status,
     createdAt: new Date(),
     updatedAt: null,
     deletedAt: null
@@ -43,27 +43,27 @@ router.put('/:id', function(req, res) {
   // where 결과 없을 시 404 필요
   const data = req.body;
   db.Refugee.update({
-    name: data['name'],
-    birth: data['birth'],
-    nationality: data['nationality'],
-    status: data['status'],
+    name: data.name,
+    birth: data.birth,
+    nationality: data.nationality,
+    status: data.status,
     updatedAt: new Date()
-  },{where: {id: req.params.id}, returning: true})
-      .then(function (results) {
-        res.json(results);
-      }).catch(function (err) {
-        res.json(err);
-      });
+  }, { where: { id: req.params.id }, returning: true })
+    .then(function (results) {
+      res.json(results);
+    }).catch(function (err) {
+      res.json(err);
+    });
 });
 
 router.delete('/:id', function (req, res) {
   // where 결과 없을 시 404 필요
-  db.Refugee.destroy({where: {id: req.params.id}})
-      .then(function(result) {
-        res.json(result);
-      }).catch(function(err) {
-        res.json(err);
-      });
+  db.Refugee.destroy({ where: { id: req.params.id } })
+    .then(function(result) {
+      res.json(result);
+    }).catch(function(err) {
+      res.json(err);
+    });
 });
 
 module.exports = router;
--- routes/visitLog.js
+++ routes/visitLog.js
@@ -4,58 +4,58 @@ const db = require('../models/index');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    db.VisitLog.findAll().then(function (results) {
-        res.json(results);
-    }).catch( function(err){
-        res.json(err);
-    });
+  db.VisitLog.findAll().then(function (results) {
+    res.json(results);
+  }).catch(function(err) {
+    res.json(err);
+  });
 });
 
 router.get('/:id', function(req, res) {
-    db.VisitLog.findOne({where: {id: req.params.id}}).then(function (results) {
-        res.json(results);
-    }).catch( function(err){
-        res.json(err);
-    });
+  db.VisitLog.findOne({ where: { id: req.params.id } }).then(function (results) {
+    res.json(results);
+  }).catch(function(err) {
+    res.json(err);
+  });
 });
 
 router.post('/', function(req, res) {
-    const data = req.body;
-    db.VisitLog.create({
-        createdAt: new Date(),
-        updatedAt: null,
-        deletedAt: null,
-        refugee_id: data['refugee_id'],
-        support: data['support']
-    }).then(function (results) {
-        res.json(results);
-    }).catch(function (err) {
-        res.json(err);
-    })
+  const data = req.body;
+  db.VisitLog.create({
+    createdAt: new Date(),
+    updatedAt: null,
+    deletedAt: null,
+    refugee_id: data.refugee_id,
+    support: data.support
+  }).then(function (results) {
+    res.json(results);
+  }).catch(function (err) {
+    res.json(err);
+  });
 });
 
 router.put('/:id', function(req, res) {
-    // 400 handling 필요, where 결과 없을 시 404 필요
-    const data = req.body;
-    db.VisitLog.update({
-        support: data['support'],
-        refugee_id: data['refugee_id'],
-        updatedAt: new Date()
-    },{where: {id: req.params.id}, returning: true})
-        .then(function (results) {
-            res.json(results);
-        }).catch(function (err) {
-            res.json(err);
+  // 400 handling 필요, where 결과 없을 시 404 필요
+  const data = req.body;
+  db.VisitLog.update({
+    support: data.support,
+    refugee_id: data.refugee_id,
+    updatedAt: new Date()
+  }, { where: { id: req.params.id }, returning: true })
+    .then(function (results) {
+      res.json(results);
+    }).catch(function (err) {
+      res.json(err);
     });
 });
 
 router.delete('/:id', function (req, res) {
-    // where 결과 없을 시 404 필요
-    db.VisitLog.destroy({where: {id: req.params.id}})
-        .then(function(result) {
-            res.json(result);
-        }).catch(function(err) {
-            res.json(err);
+  // where 결과 없을 시 404 필요
+  db.VisitLog.destroy({ where: { id: req.params.id } })
+    .then(function(result) {
+      res.json(result);
+    }).catch(function(err) {
+      res.json(err);
     });
 });
 
