756a7295ce09c40f3cf393782dd590b6433b2434
Jihwan Kim <noneobj@gmail.com>
2019-11-18 15:04:54 +0900

    Add visit log orm defination

---
+++ models/VisitLog.js
@@ -0,0 +1,34 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('VisitLog', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    refugee_id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    support: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    }
+  }, {
+    tableName: 'VisitLog'
+  });
+};
