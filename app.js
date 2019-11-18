1416e2e069557d897b4f59303d33963f438c2634
Jihwan Kim <noneobj@gmail.com>
2019-11-18 15:24:47 +0900

    Change url of visitLog

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/refugee', refugeeRouter);
-app.use('/api/v1/visit', visitLogRouter);
+app.use('/api/v1/visitlog', visitLogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
