f0ec149f1be3dd3371c9f8e31c4d453f98a278b1
Jihwan Kim <noneobj@gmail.com>
2019-11-20 17:37:04 +0900

    Add querystring filter to retrive api

--- routes/refugee.js
+++ routes/refugee.js
@@ -4,8 +4,40 @@ const db = require('../models/index');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  // 결과 없을 시 404 필요
-  db.Refugee.findAll().then(function (results) {
+  const where = {};
+  const params = req.query;
+
+  if(params.id) {
+    where.id = params.id;
+  }
+  if(params.name) {
+    where.name = decodeURI(params.name);
+  }
+  if(params.nationality) {
+    where.nationality = decodeURI(params.nationality);
+  }
+  if(params.status) {
+    where.status = decodeURI(params.status);
+  }
+  if(params.st_date && params.ed_date) {
+    const stDate = new Date(params.st_date);
+    let edDate = new Date(params.ed_date);
+    edDate = new Date(edDate.getTime() + (1000 * 60 * 60 * 24) - 1000);
+
+    where.createdAt = {
+      [db.Operator.between]: [stDate, edDate]
+    };
+  }
+  if(params.st_date && !params.ed_date) {
+    const stDate = new Date(params.st_date);
+    const oneDay = new Date(stDate.getTime() + (1000 * 60 * 60 * 24) - 1000);
+
+    where.createdAt = {
+      [db.Operator.between]: [stDate, oneDay]
+    };
+  }
+
+  db.Refugee.findAll({ where: where }).then(function (results) {
     res.json(results);
   }).catch(function(err) {
     res.json(err);
--- routes/visitLog.js
+++ routes/visitLog.js
@@ -4,7 +4,48 @@ const db = require('../models/index');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  db.VisitLog.findAll().then(function (results) {
+  const params = req.query;
+
+  const where = {};
+  const include = [{
+    model: db.Refugee
+  }];
+
+  if(params.refugee_id) {
+    where.refugee_id = params.refugee_id;
+  }
+
+  if(params.nationality) {
+    include[0].where = { nationality: decodeURI(params.nationality) };
+  }
+
+  if(params.name) {
+    include[0].where = { name: decodeURI(params.name) };
+  }
+
+  if(params.support) {
+    where.support = decodeURI(params.support);
+  }
+
+  if(params.st_date && params.ed_date) {
+    const stDate = new Date(params.st_date);
+    let edDate = new Date(params.ed_date);
+    edDate = new Date(edDate.getTime() + (1000 * 60 * 60 * 24) - 1000);
+
+    where.createdAt = {
+      [db.Operator.between]: [stDate, edDate]
+    };
+  }
+
+  if(params.st_date && !params.ed_date) {
+    const stDate = new Date(params.st_date);
+    const oneDay = new Date(stDate.getTime() + (1000 * 60 * 60 * 24) - 1000);
+
+    where.createdAt = {
+      [db.Operator.between]: [stDate, oneDay]
+    };
+  }
+  db.VisitLog.findAll({ where: where, include: include }).then(function (results) {
     res.json(results);
   }).catch(function(err) {
     res.json(err);
