bc45adefd67d50cd91a6173d3af69e6e0ead19e5
김광호 <skyhyunmi@gimgwanghoui-MacBookPro.local>
2019-12-03 14:45:03 +0900

    fix indentation

--- routes/statistic.js
+++ routes/statistic.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const db = require('../models/index');
 const util = require('../config/util');
 
-router.get('/country', (req,res,next) => {
-    db.Country_Code.findAll({}).then(function (results) {
-        res.json(results);
-      }).catch(function() {
-        res.status(404).send();
-      });
+router.get('/country', util.isLoggedin, (req, res, next) => {
+  db.Country_Code.findAll({}).then(function (results) {
+    res.json(results);
+  }).catch(function() {
+    res.status(404).send();
+  });
 });
 
 module.exports = router;
