1b901118201d143b6297697049a441c8acb72321
Skyhyunmi <50918827+Skyhyunmi@users.noreply.github.com>
2019-12-07 20:14:37 +0900

    expand the length of name of refugee

--- models/Refugee.js
+++ models/Refugee.js
@@ -25,7 +25,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: true
     },
     name: {
-      type: DataTypes.STRING(45),
+      type: DataTypes.STRING(200),
       allowNull: false
     },
     nationality: {
