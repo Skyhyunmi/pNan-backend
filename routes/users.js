3de42c7b108287c5361046451b2ee32845e32a3b
Jihwan Kim <noneobj@gmail.com>
2019-12-10 18:03:35 +0900

    Fix user put result logic error

--- routes/users.js
+++ routes/users.js
@@ -69,7 +69,7 @@ router.put('/:id', util.isLoggedin, util.isAdmin, async function (req, res) {
       },
       returning: true
     });
-    if (!result[0]) {
+    if (!result[1]) {
       return res.status(400).json(util.successFalse('no column effected'));
     }
   } catch (err) {
