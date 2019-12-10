5ce2be7a18fe80e2bb1133ad6c128fb9b68dd170
Kim Kwangho <codikh1@gmail.com>
2019-12-10 17:02:29 +0900

    save

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 const LocalStrategy = require('passport-local').Strategy;
 const db = require('../models/index');
-const crypyto = require('crypto');
+const crypto = require('crypto');
 
 module.exports = function () {
   passport.serializeUser(function (user, done) {
@@ -14,6 +14,91 @@ module.exports = function () {
   passport.deserializeUser(function (user, done) {
     done(null, user);
   });
+
+  passport.use(
+    'signup',
+    new LocalStrategy(
+      {
+        usernameField: 'id',
+        passwordField: 'pw',
+        session: false,
+        passReqToCallback: false
+      },
+      function (id, password, done) {
+        try {
+          db.User.findOne({
+            where: {
+              user_id: id
+            }
+          }).then(function (user) {
+            if (user) {
+              return done(null, false, { message: 'user already taken.' });
+            } else {
+              const salt = buf.toString('base64');
+              crypto.pbkdf2(password, salt, 100000, 64, 'sha512', function (err, key) {
+                db.User.create({
+                  user_id: id,
+                  name: data.name,
+                  email: data.email,
+                  salt: salt,
+                  admin: data.is_admin,
+                  hashed_password: key.toString('base64'),
+                  createdAt: new Date(),
+                  updatedAt: null
+                }).then(function (result) {
+                  res.json(result);
+                }).catch(function (err) {
+                  if (err.errors[0].path === 'user_id') {
+                    res.status(404).json(util.successFalse(err, '아이디 중복'));
+                  } else {
+                    res.status(404).json(util.successFalse(err, '이메일 중복'));
+                  }
+                });
+              });
+            }
+          })
+        } catch (err) {
+          done(err);
+        }
+      }
+    ))
+
+  passport.use(
+    'login',
+    new LocalStrategy(
+      {
+        usernameField: 'id',
+        passwordField: 'pw',
+        session: false,
+      },
+      function (id, password, done) {
+        try {
+          db.User.findOne({
+            where: {
+              user_id: id
+            }
+          }).then(function (user) {
+            if (user) {
+              crypto.pbkdf2(password, user.salt, 100000, 64, 'sha512', function (err, key) {
+                if (err) {
+                  done(null, false, { message: 'error' });
+                }
+                if (user.hashed_password === key.toString('base64')) {
+                  return done(null, user);
+                } else {
+                  return done(null, false, { message: 'Password do not match.' });
+                }
+              });
+            } else {
+              return done(null, false, { message: 'ID do not match' });
+            }
+          })
+        } catch (err) {
+          done(err);
+        }
+      }
+    )
+  )
 
   passport.use('local', new LocalStrategy({
     usernameField: 'id',
--- routes/auth.js
+++ routes/auth.js
@@ -2,42 +2,64 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-const crypyto = require('crypto');
+const crypto = require('crypto');
 const db = require('../models/index');
 const util = require('../config/util');
 
 require('dotenv').config();
 
+// router.post('/signup', util.isLoggedin, util.isAdmin, function (req, res) {
+//   const data = req.body;
+//   // eslint-disable-next-line handle-callback-err
+//   crypto.randomBytes(64, function (err, buf) {
+//     const salt = buf.toString('base64');
+//     let hashedPw;
+//     // eslint-disable-next-line handle-callback-err
+//     crypto.pbkdf2(data.pw, salt, 100000, 64, 'sha512', function (err, key) {
+//       hashedPw = key.toString('base64');
+//       db.User.create({
+//         user_id: data.id,
+//         name: data.name,
+//         email: data.email,
+//         salt: salt,
+//         admin: data.is_admin,
+//         hashed_password: hashedPw,
+//         createdAt: new Date(),
+//         updatedAt: null
+//       }).then(function (results) {
+//         res.json(results);
+//       }).catch(function (err) {
+//         if (err.errors[0].path === 'user_id') {
+//           res.status(404).json(util.successFalse(err, '아이디 중복'));
+//         } else {
+//           res.status(404).json(util.successFalse(err, '이메일 중복'));
+//         }
+//       });
+//     });
+//   });
+// });
+
 router.post('/signup', util.isLoggedin, util.isAdmin, function (req, res) {
-  const data = req.body;
-  // eslint-disable-next-line handle-callback-err
-  crypyto.randomBytes(64, function (err, buf) {
-    const salt = buf.toString('base64');
-    let hashedPw;
-    // eslint-disable-next-line handle-callback-err
-    crypyto.pbkdf2(data.pw, salt, 100000, 64, 'sha512', function (err, key) {
-      hashedPw = key.toString('base64');
-      db.User.create({
-        user_id: data.id,
-        name: data.name,
-        email: data.email,
-        salt: salt,
-        admin: data.is_admin,
-        hashed_password: hashedPw,
-        createdAt: new Date(),
-        updatedAt: null
-      }).then(function (results) {
-        res.json(results);
-      }).catch(function (err) {
-        if (err.errors[0].path === 'user_id') {
-          res.status(404).json(util.successFalse(err, '아이디 중복'));
-        } else {
-          res.status(404).json(util.successFalse(err, '이메일 중복'));
-        }
-      });
-    });
-  });
-});
+  passport.authenticate('register', function (err, user, info) {
+    if (err) {
+      return res.status(403).json(util.successFalse(err));
+    }
+    if (info) {
+      return res.status(403).json(util.successFalse(info));
+    } else {
+      req.logIn( function(user,err) {
+        const data = req.body;
+        User.findOne({
+          where: {
+            user_id:data.id
+          }
+        }).then( function (user) {
+          
+        })
+      })
+    }
+  })
+})
 
 router.post('/login', function (req, res, next) {
   passport.authenticate('local', { session: false }, function (err, user) {
