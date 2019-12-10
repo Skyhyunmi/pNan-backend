34d63ff7c6a2efc4b84b69e1da56443a1e888809
Jihwan Kim <noneobj@gmail.com>
2019-12-10 17:06:17 +0900

    Reformatted User and Fix functions

--- routes/users.js
+++ routes/users.js
@@ -1,120 +1,104 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const crypyto = require('crypto');
+const moment = require('moment');
+const crypto = require('crypto');
 const util = require('../config/util');
 const db = require('../models/index');
+
+function getDate(date) {
+  if (date) {
+    moment(date).format('YYYY-MM-DD HH:mm:ss');
+  } else {
+    return moment().format('YYYY-MM-DD HH:mm:ss');
+  }
+}
 
 router.get('/', util.isLoggedin, function (req, res) {
   db.User.findAll().then(function (result) {
     res.json(result);
   }).catch(function (err) {
-    res.status(404).json(util.successFalse(err));
+    res.status(500).json(util.successFalse(err));
   });
 });
 
 router.get('/:id', util.isLoggedin, util.isAdmin, function (req, res) {
   db.User.findOne({
-    where: { user_id: req.params.id }
+    where: { id: req.params.id }
   }).then(function (result) {
-    if (result) res.json(result);
-    else res.status(404).json(util.successFalse(null, 'Not valid user id'));
+    if (result) {
+      res.json(result);
+    } else {
+      res.status(404).json(util.successFalse(null, 'there is no user'));
+    }
   }).catch(function (err) {
-    res.status(404).json(util.successFalse(err));
+    res.status(500).json(util.successFalse(err));
   });
 });
 
-router.put('/:id', util.isLoggedin, function (req, res) {
-  var data = req.body;
-  if (req.decoded.id !== req.params.id) {
-    res.status(403).json(util.successFalse(null, 'Don\'t have permission'));
-  } else if (data.new_pw) {
-    crypyto.randomBytes(64, function (err, buf) {
-      if (err) res.status(404).json(util.successFalse(err));
-      const salt = buf.toString('base64');
-      let hashedPw;
-      // eslint-disable-next-line handle-callback-err
-      crypyto.pbkdf2(data.new_pw, salt, 100000, 64, 'sha512', function (err, key) {
-        hashedPw = key.toString('base64');
-        db.User.update({
-          name: data.name,
-          email: data.email,
-          salt: salt,
-          admin: data.admin,
-          hashed_password: hashedPw,
-          updatedAt: new Date()
-        }, {
-          where: { user_id: req.decoded.id }, returning: true
-        }).then(function () {
-          db.User.findOne({
-            where: { user_id: req.decoded.id }
-          }).then(function (user) {
-            if (!user) res.status(404).json(util.successFalse(null, 'Not valid user id'));
-            else {
-              const payload = {
-                id: user.user_id,
-                name: user.name,
-                admin: user.admin,
-                loggedAt: new Date()
-              };
-              const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 60 * 90 });
-              user.authToken = token;
-              res.json({ token: token, admin: user.admin });
-            }
-          });
-        }).catch(function (err) {
-          res.status(404).json(util.successFalse(err));
-        });
-      });
-    });
-  } else {
-    db.User.update({
-      name: data.name,
-      email: data.email,
-      admin: data.admin,
-      updatedAt: new Date()
-    }, {
-      where: { user_id: req.decoded.id }
-    }).then(function () {
-      db.User.findOne({
-        where: { user_id: req.decoded.id }
-      }).then(function (user) {
-        if (!user) res.status(404).json(util.successFalse(null, 'Not valid user id'));
-        else {
-          const payload = {
-            id: user.user_id,
-            name: user.name,
-            admin: user.admin,
-            loggedAt: new Date()
-          };
-          const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 60 * 90 });
-          user.authToken = token;
-          res.json({ token: token, admin: user.admin });
-        }
-      });
-    }).catch(function (err) {
-      res.status(404).json(util.successFalse(err));
-    });
+router.put('/:id', util.isLoggedin, util.isAdmin, async function (req, res) {
+  const data = req.body;
+  const user = await db.User.findOne({
+    where: { id: req.params.id }
+  });
+
+  let hashedPw = null;
+  let salt = null;
+  try {
+    if (data.new_pw) {
+      const buffer = crypto.randomBytes(64);
+      salt = buffer.toString('base64');
+      const key = crypto.pbkdf2Sync(data.new_pw, salt, 100000, 64, 'sha512');
+      hashedPw = key.toString('base64');
+    }
+  } catch (err) {
+    res.status(500).json(util.successFalse(err));
   }
+  let result = null;
+  try {
+    result = await db.User.update({
+      user_id: data.user_id ? data.user_id : user.user_id,
+      name: data.name ? data.name : user.name,
+      email: data.email ? data.email : user.email,
+      admin: data.admin === undefined ? data.admin : user.admin,
+      hashed_password: data.new_pw ? hashedPw : user.hashed_password,
+      salt: data.new_pw ? salt : user.salt,
+      updatedAt: getDate()
+    }, {
+      where: {
+        id: req.params.id
+      },
+      returning: true
+    });
+    if (!result[1]) {
+      res.status(400).json(util.successFalse('no column effected'));
+    }
+  } catch (err) {
+    res.status(500).json(util.successFalse(err));
+  }
+  res.json(result);
 });
 
 router.delete('/:id', util.isLoggedin, util.isAdmin, function (req, res) {
   db.User.findOne({
-    where: { user_id: req.params.id, admin: 0 }
-  }).then(function (rest) {
-    if (!rest) res.json(util.successFalse(null, '없는 ID거나 관리자 계정입니다.'));
-    else {
+    where: { id: req.params.id, admin: 0 }
+  }).then(function (targetUser) {
+    if (!targetUser) {
+      res.json(util.successFalse(null, 'no such user or it is admin account'));
+    } else {
       db.User.destroy({
-        where: { user_id: req.params.id }
+        where: { user_id: targetUser.id }
       }).then(function (result) {
-        if (result === 0) res.status(404).json(util.successFalse(null, 'No User is deleted.'));
-        else res.json(util.successTrue(null));
+        if (result === 0) {
+          res.status(404).json(util.successFalse(null, 'No User is deleted.'));
+        } else {
+          res.json(util.successTrue(null));
+        }
       }).catch(function (err) {
         res.status(404).json(util.successFalse(err));
       });
     }
   }).catch(function (err) {
-    res.status(404).json(util.successFalse(err));
+    res.status(400).json(util.successFalse(err, 'no such user or it is admin account'));
   });
 });
 
