c36f41595ca32f18fd907970d8b39a9fdb7a379a
Kim Kwangho <codikh1@gmail.com>
2019-12-11 22:40:24 +0900

    Delete unused util function

--- config/util.js
+++ config/util.js
@@ -17,26 +17,10 @@ util.successFalse = function (err, message, data) {
   return {
     success: false,
     message: message,
-    errors: (err) ? util.parseError(err) : null,
+    errors: err? err: null,
     data: data
   };
 };
-
-util.parseError = function (errors) {
-  var parsed = {};
-  if (errors.name === 'ValidationError') {
-    for (var name in errors.errors) {
-      var validationError = errors.errors[name];
-      parsed[name] = { message: validationError.message };
-    }
-  } else if (errors.code === '11000' && errors.errmsg.indexOf('username') > 0) {
-    parsed.username = { message: 'This username already exists!' };
-  } else {
-    parsed.unhandled = errors;
-  }
-  return parsed;
-};
-
 
 // middlewares
 util.isLoggedin = function (req, res, next) {
